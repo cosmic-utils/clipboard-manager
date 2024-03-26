85aae37420c25e0991ff84a515003941798a9ad1
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-03-26 23:03:10 +0100

    vv

--- src/view.rs
+++ src/view.rs
@@ -4,36 +4,32 @@ use cosmic::{
     iced::{Alignment, Length, Padding},
     iced_widget::{column, graphics::image::image_rs::flat::View, Row, Scrollable},
     theme,
-    widget::{
-        icon::{self, Handle},
-        mouse_area, text, text_input, Button, Column, Container, Icon, MouseArea, Space,
-    },
+    widget::{icon::{self, Handle}, mouse_area, text, text_input, Button, Column, Container, Icon, MouseArea, Space},
     Element,
 };
 
 use crate::{
-    app::{AppState, ClipboardState},
-    db::Data,
-    message::AppMessage,
-    utils::formated_value,
+    app::{AppState, ClipboardState}, db::Data, message::AppMessage, utils::formated_value
 };
 
+
+
+
+
 impl AppState {
+
     pub fn view(&self) -> Element<AppMessage> {
-        let mut elements = Vec::new();
+        
+        let content = Column::new()
+            .push(self.top_view())
+            .push(Space::with_height(20))
+            .padding(Padding::new(10f32));
 
-        elements.push(self.top_view().into());
-        elements.push(Space::with_height(20).into());
-
-        if self.query.is_empty() {
-            elements.push(Self::entry_list_view(self.db.iter().rev()));
+        let content = if self.query.is_empty() {
+            content.push(Self::entry_list_view(self.db.iter().rev()))
         } else {
-            elements.push(Self::entry_list_view(
-                self.db.search(&self.query).iter().copied(),
-            ));
-        }
-
-        let content = Column::with_children(elements).padding(Padding::new(10f32));
+            content.push(Self::entry_list_view(self.db.search(&self.query).iter().copied()))
+        };
 
         mouse_area(content)
             .on_release(AppMessage::TogglePopup)
@@ -46,25 +42,24 @@ impl AppState {
     where
         I: Iterator<Item = &'a Data>,
     {
+
         fn entry_view(data: &Data) -> Element<AppMessage> {
             let delete_button = Button::new(text("Delete"))
                 .on_press(AppMessage::Delete(data.clone()))
                 .style(theme::Button::Destructive);
-
+        
             let content = Row::new()
                 .align_items(Alignment::Center)
                 .push(
                     // todo: remove this fixed size
-                    Container::new(
-                        text(formated_value(&data.value, 2, 50)).width(Length::Fixed(300f32)),
-                    ),
+                    Container::new(text(formated_value(&data.value, 2, 50)).width(Length::Fixed(300f32))),
                 )
                 .push(Space::with_width(Length::Fill))
                 .push(delete_button)
                 .padding(5f32);
-
+        
             let card = Container::new(content).style(cosmic::theme::Container::Card);
-
+        
             MouseArea::new(card)
                 .on_release(AppMessage::OnClick(data.clone()))
                 .into()
@@ -79,7 +74,12 @@ impl AppState {
             .into()
     }
 
+    
+    
+    
+    
     fn top_view(&self) -> Element<AppMessage> {
+
         let mut row = Vec::new();
 
         let text_input = text_input::search_input("value", &self.query)
@@ -93,7 +93,7 @@ impl AppState {
         if self.clipboard_state == ClipboardState::Error {
             let icon_bytes = include_bytes!("../resources/icons/sync_problem24.svg") as &[u8];
 
-            // let a = Cow::Borrowed(icon_bytes);
+           // let a = Cow::Borrowed(icon_bytes);
 
             let icon = icon::from_svg_bytes(icon_bytes);
 
