876c73d228c24e7a85e1e39ef7265e1e81d106ca
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-04-28 02:12:18 +0200

    init (#34)

--- src/config.rs
+++ src/config.rs
@@ -6,14 +6,25 @@ use cosmic::{
 };
 
 use serde::{Deserialize, Serialize};
+use time::Duration;
 
-use crate::{app::APP_ID, message::AppMessage};
+use crate::{app::APP_ID, message::AppMessage, utils};
 
 pub const CONFIG_VERSION: u64 = 1;
 
-#[derive(CosmicConfigEntry, Debug, Clone, Default, Eq, PartialEq, Deserialize, Serialize)]
+#[derive(CosmicConfigEntry, Debug, Clone, Eq, PartialEq, Deserialize, Serialize)]
 pub struct Config {
     pub private_mode: bool,
+    pub remove_old_entries: Option<Duration>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            private_mode: false,
+            remove_old_entries: Some(time::Duration::days(30)),
+        }
+    }
 }
 
 pub static PRIVATE_MODE: AtomicBool = AtomicBool::new(false);
