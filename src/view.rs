00ba696bdcf89310b74fdc2d38916d17765168c4
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-07-16 22:26:03 +0200

    ct (#55)

--- src/view.rs
+++ src/view.rs
@@ -86,14 +86,7 @@ fn entries(state: &AppState) -> Element<'_, AppMessage> {
                         if text.is_empty() {
                             None
                         } else {
-                            let more_action = if let Some(d) = &state.more_action {
-                                d == data
-                            } else {
-                                false
-                            };
-
-                            let e = entry(data, pos == state.focused, more_action, text);
-                            Some(e)
+                            Some(entry(data, pos == state.focused, text))
                         }
                     }
                 },
@@ -113,95 +106,62 @@ fn entries(state: &AppState) -> Element<'_, AppMessage> {
         .into()
 }
 
-fn entry<'a>(
-    entry: &'a Data,
-    is_focused: bool,
-    more_action_expanded: bool,
-    content: &'a str,
-) -> Element<'a, AppMessage> {
+fn entry<'a>(entry: &'a Data, is_focused: bool, content: &'a str) -> Element<'a, AppMessage> {
     let content = text(formated_value(content, 5, 200));
 
-    let btn = mouse_area(
-        cosmic::widget::button(content)
-            .width(Length::Fill)
-            .on_press(AppMessage::Copy(entry.clone()))
-            .padding([8, 16])
-            .style(Button::Custom {
-                active: Box::new(move |focused, theme| {
-                    let rad_s = theme.cosmic().corner_radii.radius_s;
-                    let focused = is_focused || focused;
+    let btn = cosmic::widget::button(content)
+        .width(Length::Fill)
+        .on_press(AppMessage::Copy(entry.clone()))
+        .padding([8, 16])
+        .style(Button::Custom {
+            active: Box::new(move |focused, theme| {
+                let rad_s = theme.cosmic().corner_radii.radius_s;
+                let focused = is_focused || focused;
 
-                    let a = if focused {
-                        button::StyleSheet::hovered(theme, focused, focused, &Button::Text)
-                    } else {
-                        button::StyleSheet::active(theme, focused, focused, &Button::Standard)
-                    };
-                    button::Appearance {
-                        border_radius: rad_s.into(),
-                        outline_width: 0.0,
-                        ..a
-                    }
-                }),
-                hovered: Box::new(move |focused, theme| {
-                    let focused = is_focused || focused;
-                    let rad_s = theme.cosmic().corner_radii.radius_s;
-
-                    let text = button::StyleSheet::hovered(theme, focused, focused, &Button::Text);
-                    button::Appearance {
-                        border_radius: rad_s.into(),
-                        outline_width: 0.0,
-                        ..text
-                    }
-                }),
-                disabled: Box::new(|theme| button::StyleSheet::disabled(theme, &Button::Text)),
-                pressed: Box::new(move |focused, theme| {
-                    let focused = is_focused || focused;
-                    let rad_s = theme.cosmic().corner_radii.radius_s;
-
-                    let text = button::StyleSheet::pressed(theme, focused, focused, &Button::Text);
-                    button::Appearance {
-                        border_radius: rad_s.into(),
-                        outline_width: 0.0,
-                        ..text
-                    }
-                }),
+                let a = if focused {
+                    button::StyleSheet::hovered(theme, focused, focused, &Button::Text)
+                } else {
+                    button::StyleSheet::active(theme, focused, focused, &Button::Standard)
+                };
+                button::Appearance {
+                    border_radius: rad_s.into(),
+                    outline_width: 0.0,
+                    ..a
+                }
             }),
+            hovered: Box::new(move |focused, theme| {
+                let focused = is_focused || focused;
+                let rad_s = theme.cosmic().corner_radii.radius_s;
+
+                let text = button::StyleSheet::hovered(theme, focused, focused, &Button::Text);
+                button::Appearance {
+                    border_radius: rad_s.into(),
+                    outline_width: 0.0,
+                    ..text
+                }
+            }),
+            disabled: Box::new(|theme| button::StyleSheet::disabled(theme, &Button::Text)),
+            pressed: Box::new(move |focused, theme| {
+                let focused = is_focused || focused;
+                let rad_s = theme.cosmic().corner_radii.radius_s;
+
+                let text = button::StyleSheet::pressed(theme, focused, focused, &Button::Text);
+                button::Appearance {
+                    border_radius: rad_s.into(),
+                    outline_width: 0.0,
+                    ..text
+                }
+            }),
+        });
+
+    context_menu(
+        btn,
+        Some(vec![menu::Tree::new(
+            button(text(fl!("delete_entry")))
+                .on_press(AppMessage::Delete(entry.clone()))
+                .width(Length::Fill)
+                .style(Button::Destructive),
+        )]),
     )
-    .on_right_release(AppMessage::MoreAction(Some(entry.clone())));
-
-    if more_action_expanded {
-        let overlay = Column::new()
-            .push(
-                button(text(fl!("delete_entry")))
-                    .on_press(AppMessage::Delete(entry.clone()))
-                    .width(Length::Fill)
-                    .style(Button::Destructive),
-            )
-            .spacing(15)
-            .padding(7);
-
-        let overlay = container(overlay).style(theme::Container::Dropdown);
-
-        // todo: use it when the offset issue is resolved
-        // https://github.com/pop-os/libcosmic/issues/516
-        // context_menu(
-        //     btn,
-        //     Some(vec![menu::Tree::new(
-        //         button(text(fl!("delete_entry")))
-        //             .on_press(AppMessage::Delete(entry.clone()))
-        //             .width(Length::Fill)
-        //             .style(Button::Destructive),
-        //     )]),
-        // )
-        // .into()
-
-        // todo: change it by a context menu instead
-        my_widgets::drop_down::DropDown::new(btn, overlay, true)
-            .on_dismiss(AppMessage::MoreAction(None))
-            .alignment(my_widgets::alignment::Alignment::Bottom)
-            .width(Length::Fixed(180.0))
-            .into()
-    } else {
-        btn.into()
-    }
+    .into()
 }
